0ba11c70f38a2db3142fedd4b4724a289370fc1f
Kavineash JB <kavineashcsc@gmail.com>
2025-08-13 19:14:57 +0530

    update MoodOuput

--- MoodMail/src/components/MoodOutput.tsx
+++ MoodMail/src/components/MoodOutput.tsx
@@ -3,21 +3,32 @@ import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 
 type Props = {
+  color: string;
   subject: string;
   footer: string;
   onReset: () => void;
 };
 
-const MoodOutput = ({ subject, footer, onReset }: Props) => {
+const MoodOutput = ({ color, subject, footer, onReset }: Props) => {
   return (
     <div className="space-y-4 my-3">
       <div>
         <label className="block font-medium mb-1">Subject</label>
-        <Input value={subject} readOnly />
+        <Input
+          style={{ color: color }}
+          className="font-semibold"
+          value={subject}
+          readOnly
+        />
       </div>
       <div>
         <label className="block font-medium mb-1">Footer Signature</label>
-        <Textarea value={footer} readOnly />
+        <Textarea
+          style={{ color: color }}
+          className="font-semibold"
+          value={footer}
+          readOnly
+        />
       </div>
 
       <Button
