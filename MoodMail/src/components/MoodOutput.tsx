40d5e9111d26b8b982611e1341e4213222151c41
Kavineash JB <kavineashcsc@gmail.com>
2025-08-14 18:22:56 +0530

    update MoodOutput

--- MoodMail/src/components/MoodOutput.tsx
+++ MoodMail/src/components/MoodOutput.tsx
@@ -1,6 +1,9 @@
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
+import { MdContentCopy } from "react-icons/md";
+import { FaCheck } from "react-icons/fa6";
+import { useState } from "react";
 
 type Props = {
   color: string;
@@ -10,16 +13,48 @@ type Props = {
 };
 
 const MoodOutput = ({ color, subject, footer, onReset }: Props) => {
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  const date = new Date();
+  const fullDate =
+    date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear();
+
+  function handleCopy() {
+    navigator.clipboard
+      .writeText(subject)
+      .then(() => {
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 3000);
+      })
+      .catch((err) => console.error("Faied to copy ", err));
+  }
+
   return (
     <div className="space-y-4 my-3">
       <div>
-        <label className="block font-medium mb-1">Subject</label>
-        <Input
-          style={{ color: color }}
-          className="font-semibold"
-          value={subject}
-          readOnly
-        />
+        <div className="flex justify-between items-center">
+          <label className="block font-medium mb-1">Subject</label>
+          <label className="bg-gray-500 text-white rounded-sm p-1 text-[12px] mb-2">
+            {fullDate}
+          </label>
+        </div>
+        <div className="relative">
+          <Input
+            style={{ color: color }}
+            className="relative font-semibold"
+            value={subject}
+            readOnly
+          />
+
+          {!isCopied ? (
+            <MdContentCopy
+              onClick={handleCopy}
+              className="absolute right-0 top-1/2 -translate-y-1/2 mx-2 cursor-pointer"
+            />
+          ) : (
+            <FaCheck className="absolute right-0 top-1/2 -translate-y-1/2 mx-2 cursor-pointer" />
+          )}
+        </div>
       </div>
       <div>
         <label className="block font-medium mb-1">Footer Signature</label>
