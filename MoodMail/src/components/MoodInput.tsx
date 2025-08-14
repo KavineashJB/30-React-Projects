a4fa73c1391723c2c7ac811bca213e50b31c54cc
Kavineash JB <kavineashcsc@gmail.com>
2025-08-14 18:22:30 +0530

    update MoodInput

--- MoodMail/src/components/MoodInput.tsx
+++ MoodMail/src/components/MoodInput.tsx
@@ -1,6 +1,6 @@
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-
+import { useEffect, useRef } from "react";
 type Props = {
   mood: string;
   setMood: (val: string) => void;
@@ -9,9 +9,15 @@ type Props = {
 };
 
 const MoodInput = ({ mood, setMood, onGenerate, disabled }: Props) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    inputRef?.current?.focus();
+  }, []);
   return (
     <div className="space-y-4">
       <Input
+        ref={inputRef}
         placeholder="Hey! What's your mood today..."
         value={mood}
         onChange={(e) => setMood(e.target.value)}
