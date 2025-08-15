2f1e0142c6daf6301c6447382e9d16e25e2bc1e5
Kavineash JB <kavineashcsc@gmail.com>
2025-08-15 14:00:55 +0530

    update TimeChart

--- TimeTracker/src/components/TimeChart.tsx
+++ TimeTracker/src/components/TimeChart.tsx
@@ -1,5 +1,6 @@
 import { Pie } from "react-chartjs-2";
 import { Chart as chartjs, ArcElement, Tooltip, Legend } from "chart.js";
+import { useEffect, useState } from "react";
 
 chartjs.register(ArcElement, Tooltip, Legend);
 
@@ -8,26 +9,32 @@ type Props = {
 };
 
 const TimeChart = ({ data }: Props) => {
+  const [colors, setColors] = useState<string[]>([]);
+
+  // If there is any change in the data (new activity added) then generate a random color for that activity
+  useEffect(() => {
+    const color: string = `#${Math.floor(Math.random() * 16777215).toString(
+      16
+    )}`;
+    setColors([...colors, color]);
+  }, [data]);
+
   const chartData = {
-    labels: data.map((d) => d.activity),
+    labels: data.map(
+      (d) => d.activity.at(0)?.toUpperCase() + d.activity.slice(1)
+    ),
     datasets: [
       {
         label: "Hours",
         data: data.map((d) => d.hrs),
-        backgroundColor: [
-          "#FF6384",
-          "#36A2EB",
-          "#FFCE56",
-          "#34D399",
-          "#A7BBFA",
-          "#A788FA",
-        ],
+        backgroundColor: [...colors],
         borderWidth: 1,
       },
     ],
   };
 
-  return <Pie data={chartData} />;
+  // key property used to reload the Pie if there is any change in the data
+  return <Pie key={JSON.stringify(data)} data={chartData} />;
 };
 
 export default TimeChart;
