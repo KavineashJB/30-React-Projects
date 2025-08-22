318fceb8c35be1ecc304d81e07962350fe8aa77d
Kavineash JB <kavineashcsc@gmail.com>
2025-08-22 23:04:47 +0530

    Update Home.tsx

--- NoteNest/src/pages/Home.tsx
+++ NoteNest/src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import NoteForm from "@/components/NoteForm";
 import NotesList from "@/components/NotesList";
 import { useState } from "react";
+import { Slide, toast, ToastContainer } from "react-toastify";
+
+// for successful deletion, toast() not showing why? resolve this.
+// once changing filter to default without erasing content. It also occur in the default note input value
 
 const Home = () => {
   const [isEditable, setIsEditble] = useState<boolean>(false);
   const [editableId, setEditableId] = useState<string>("");
+  const [isFilter, setIsFilter] = useState<boolean>(false);
+  const [filterNoteText, setFilterNoteText] = useState<string>("");
 
   function handleIsEditable(id: string) {
     // toggle the editability because this function is handled by both NoteForm and NoteList Components
@@ -18,6 +24,21 @@ const Home = () => {
     setEditableId(id);
   }
 
+  function handleIsFilter() {
+    setIsFilter((prev) => !prev);
+    if (isFilter) {
+      toast.info("Filter Removed!");
+      setFilterNoteText("");
+    } else {
+      toast.info("Filter Notes by Tags");
+    }
+  }
+
+  function handleFilterNoteText(text: string) {
+    if (isFilter) setFilterNoteText(text);
+    else setFilterNoteText("");
+  }
+
   return (
     <div>
       <div className="w-sm space-y-5 shadow-[0_0_10px_0_rgba(0,0,0,0.2)] p-6 rounded-sm">
@@ -25,10 +46,25 @@ const Home = () => {
         <NoteForm
           isEditable={isEditable}
           editableId={editableId}
+          isFilter={isFilter}
           handleIsEditable={handleIsEditable}
+          handleIsFilter={handleIsFilter}
+          handleFilterNoteText={handleFilterNoteText}
         />
       </div>
-      <NotesList isEditable={isEditable} handleIsEditable={handleIsEditable} />
+      <NotesList
+        isEditable={isEditable}
+        filterNoteText={filterNoteText}
+        handleIsEditable={handleIsEditable}
+      />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={true}
+        closeOnClick
+        theme="light"
+        transition={Slide}
+      />
     </div>
   );
 };
